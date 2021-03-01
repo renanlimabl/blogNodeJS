38d541e6171e5d04df4d53ebdf36eddf62063d19
Renan Lima <renanlimabl@gmail.com>
2021-03-01 04:35:48 -0300

    add relacionamento article x category

--- articles/Article.js
+++ articles/Article.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const connection = require('../database/database')
+const Category = require('../categories/Category')
 
 const Article = connection.define('articles', {
   title: {
@@ -15,5 +16,12 @@ const Article = connection.define('articles', {
     allowNull: false
   }
 })
+
+// 1 - p - N (hasMany)
+Category.hasMany(Article) // 1 categoria - N artigos
+// 1 - p - 1 (belongsTo)
+Article.belongsTo(Category) // 1 artigo - 1 categoria
+
+// Article.sync({ force: true })
 
 module.exports = Article
